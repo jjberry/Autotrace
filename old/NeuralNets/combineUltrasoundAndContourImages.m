449be174963d90360f6d7bf802b241d3f93a16f8
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-09-01 09:53:51 -0700

    Modified combineUltrasoundAndContourImages.m to test whether or not image is already grayscale

--- old/NeuralNets/combineUltrasoundAndContourImages.m
+++ old/NeuralNets/combineUltrasoundAndContourImages.m
@@ -1,15 +1,20 @@
 function [XC, m, s, height, width, continds] = combineUltrasoundAndContourImages(contfiles, id, contimgs, datadir, roi)
-  
+
   top = roi(1);
   bottom = roi(2);
   left = roi(3);
   right = roi(4)
 
-  directory = fullfile(datadir,['Subject' num2str(id(1))], 'IMAGES')  
+  directory = fullfile(datadir,['Subject' num2str(id(1))], 'IMAGES')
   % Read one image to see what the final size will be when we resize it
   scale = 0.1;
   img = imread( fullfile(directory, contfiles{1}) );
-  cropped = double(imresize(rgb2gray(img(top:bottom,left:right,:)),scale,'bicubic'));
+
+  % test to see whether or not image is grayscale
+  if size(img, 3) == 3
+    img = rgb2gray(img(top:bottom,left:right,:));
+  end
+  cropped = double(imresize(img, scale,'bicubic'));
   [height, width] = size(cropped);
 
   % Set things up to remove the always zero elements from the contours
@@ -27,26 +32,31 @@ function [XC, m, s, height, width, continds] = combineUltrasoundAndContourImages
       % For all images with a contour, load and crop and grayscale the image
       directory = fullfile(datadir,['Subject' num2str(id(j))], 'IMAGES');
       img = imread( fullfile(directory,contfiles{j}) );
-      cropped = double(imresize(rgb2gray(img(top:bottom,left:right,:)),scale,'bicubic'));
+
+      % test to see whether or not image is grayscale
+      if size(img, 3) == 3
+        img = rgb2gray(img(top:bottom,left:right,:));
+      end
+      cropped = double(imresize(img,scale,'bicubic'));
 
       % subplot(3,1,1)
       % imshowtrue(cropped);
 
       % Scale and concatenate the contour data
       cont = imresize(contimgs{j},[height,width],'bicubic');
-      
+
       % subplot(3,1,2)
       % imshowtrue(cont);
-      
+
       s = max(cont);
       s(s<0.01) = 1;
       cont = cont ./ repmat(s,[height,1]);
       cont(cont<0) = 0;
-      
+
       % subplot(3,1,3)
       % imshowtrue(cont);
       % pause
-      
+
       XC(j,:) = [cropped(:)', cont(continds)'];
   end
 
