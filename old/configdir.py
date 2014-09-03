621425f4ccd75310a19380dfae005bea646e3a97
Gus Hahn-Powell <gushahnpowell@gmail.com>
2014-09-03 15:09:01 -0700

    handling of grayscales in Matlab failed, so the work has been moved to configdir.py

--- old/configdir.py
+++ old/configdir.py
@@ -11,23 +11,23 @@ purpose:
 
 usage:
 	python configdir.py
-	
-	This script now launches a GUI that will allow the user to select a directory 
-	containing .jpg/.png and corresponding .traced.txt files, 
+
+	This script now launches a GUI that will allow the user to select a directory
+	containing .jpg/.png and corresponding .traced.txt files,
 	as well as ROI_config.txt (optional).
-	
+
 	The contents of this directory will be organized into the following format:
-	
+
 	Subject1
 	--> TongueContours.csv
 	--> IMAGES
 		 --> jpg and/or png files
-		  
+
 	--> traces
 		 --> traced.txt files
 
 	--> ROI_config.txt (if present)
-	
+
 --------------------------------------------------
 Modified by Jeff Berry on Feb 19 2011
 reason:
@@ -36,6 +36,7 @@ reason:
 
 import Tkinter, Tkconstants, tkFileDialog, tkMessageBox
 import os, subprocess, re, sys
+from PIL import Image #for grayscale image preparation
 
 image_extension_pattern = re.compile("\.(jpg|png)$", re.IGNORECASE)
 
@@ -53,11 +54,11 @@ class TkFileDialogExample(Tkinter.Frame):
 
 		# button optoins
 		button_opt = {'fill': Tkconstants.BOTH, 'padx': 10, 'pady': 10}
-		
+
 		# buttons
 		Tkinter.Button(self, text='Select Directory', command=self.askdirectory).pack(**button_opt)
 		Tkinter.Button(self, text="Quit", command=sys.exit).pack(**button_opt)
-		
+
 		# options for opening directory
 		self.dir_opt = options = {}
 		options['initialdir'] = os.path.expanduser("~")
@@ -70,54 +71,67 @@ class TkFileDialogExample(Tkinter.Frame):
 		"""Returns a selected directory name."""
 
 		self.dir = tkFileDialog.askdirectory(**self.dir_opt)
+		# move to specified directory
 		os.chdir(self.dir)
 		self.configdir()
 
 
 	def formatAutoTrace(self):
 			filename = 'Subject1/TongueContours.csv'
-			o = open(filename, 'w')
-			o.write("Filename\tRaw.R1.X\tRaw.R1.Y\tRaw.R2.X\tRaw.R2.Y\tRaw.R3.X\tRaw.R3.Y\tRaw.R4.X\tRaw.R4.Y\tRaw.R5.X\tRaw.R5.Y\tRaw.R6.X\tRaw.R6.Y\tRaw.R7.X\tRaw.R7.Y\tRaw.R8.X\tRaw.R8.Y\tRaw.R9.X\tRaw.R9.Y\tRaw.R10.X\tRaw.R10.Y\tRaw.R11.X\tRaw.R11.Y\tRaw.R12.X\tRaw.R12.Y\tRaw.R13.X\tRaw.R13.Y\tRaw.R14.X\tRaw.R14.Y\tRaw.R15.X\tRaw.R15.Y\tRaw.R16.X\tRaw.R16.Y\tRaw.R17.X\tRaw.R17.Y\tRaw.R18.X\tRaw.R18.Y\tRaw.R19.X\tRaw.R19.Y\tRaw.R20.X\tRaw.R20.Y\tRaw.R21.X\tRaw.R21.Y\tRaw.R22.X\tRaw.R22.Y\tRaw.R23.X\tRaw.R23.Y\tRaw.R24.X\tRaw.R24.Y\tRaw.R25.X\tRaw.R25.Y\tRaw.R26.X\tRaw.R26.Y\tRaw.R27.X\tRaw.R27.Y\tRaw.R28.X\tRaw.R28.Y\tRaw.R29.X\tRaw.R29.Y\tRaw.R30.X\tRaw.R30.Y\tRaw.R31.X\tRaw.R31.Y\tRaw.R32.X\tRaw.R32.Y\tUL.x\tUL.y\tUR.x\tUR.y\tLL.x\tLL.y\tLR.x\tLR.y\tAux1.x\tAux1.y\tAux2.x\tAux2.y\tAux3.x\tAux3.y\tAux4.x\tAux4.y\tAux5.x\tAux5.y\tAux6.x\tAux6.y\tAux7.x\tAux7.y\tAux8.x\tAux8.y\tAux9.x\tAux9.y\tAux10.x\tAux10.y\n")
-			
-			fnamesl = sorted(os.listdir('.'))
-			fnames = []
-			for i in fnamesl:
-					if '.traced.txt' in i:
-							fnames.append(i)
-			warnings = 0    
-			for i in fnames:
-					f = open(i, 'r').readlines()
-					if (len(f) != 32):
-							print '\033[91m' + 'Warning: ' + i + ' does not have 32 lines\n\tThis will cause an error during training' + '\033[0m'
-							warnings += 1
-					nums = []
-					for j in range(len(f)):
-							x = f[j][:-1].split('\t')
-							nums.append(int(round(float(x[1]))))
-							nums.append(int(round(float(x[2]))))
+			with open(filename, 'w') as o:
+				o.write("Filename\tRaw.R1.X\tRaw.R1.Y\tRaw.R2.X\tRaw.R2.Y\tRaw.R3.X\tRaw.R3.Y\tRaw.R4.X\tRaw.R4.Y\tRaw.R5.X\tRaw.R5.Y\tRaw.R6.X\tRaw.R6.Y\tRaw.R7.X\tRaw.R7.Y\tRaw.R8.X\tRaw.R8.Y\tRaw.R9.X\tRaw.R9.Y\tRaw.R10.X\tRaw.R10.Y\tRaw.R11.X\tRaw.R11.Y\tRaw.R12.X\tRaw.R12.Y\tRaw.R13.X\tRaw.R13.Y\tRaw.R14.X\tRaw.R14.Y\tRaw.R15.X\tRaw.R15.Y\tRaw.R16.X\tRaw.R16.Y\tRaw.R17.X\tRaw.R17.Y\tRaw.R18.X\tRaw.R18.Y\tRaw.R19.X\tRaw.R19.Y\tRaw.R20.X\tRaw.R20.Y\tRaw.R21.X\tRaw.R21.Y\tRaw.R22.X\tRaw.R22.Y\tRaw.R23.X\tRaw.R23.Y\tRaw.R24.X\tRaw.R24.Y\tRaw.R25.X\tRaw.R25.Y\tRaw.R26.X\tRaw.R26.Y\tRaw.R27.X\tRaw.R27.Y\tRaw.R28.X\tRaw.R28.Y\tRaw.R29.X\tRaw.R29.Y\tRaw.R30.X\tRaw.R30.Y\tRaw.R31.X\tRaw.R31.Y\tRaw.R32.X\tRaw.R32.Y\tUL.x\tUL.y\tUR.x\tUR.y\tLL.x\tLL.y\tLR.x\tLR.y\tAux1.x\tAux1.y\tAux2.x\tAux2.y\tAux3.x\tAux3.y\tAux4.x\tAux4.y\tAux5.x\tAux5.y\tAux6.x\tAux6.y\tAux7.x\tAux7.y\tAux8.x\tAux8.y\tAux9.x\tAux9.y\tAux10.x\tAux10.y\n")
 
-					for k in range(8):
-							nums.append(-1)
+				fnamesl = sorted(os.listdir('.'))
+				fnames = []
+				for i in fnamesl:
+						if '.traced.txt' in i:
+								fnames.append(i)
+				warnings = 0
+				for i in fnames:
+						f = open(i, 'r').readlines()
+						if (len(f) != 32):
+								print '\033[91m' + 'Warning: ' + i + ' does not have 32 lines\n\tThis will cause an error during training' + '\033[0m'
+								warnings += 1
+						nums = []
+						for j in range(len(f)):
+								x = f[j][:-1].split('\t')
+								nums.append(int(round(float(x[1]))))
+								nums.append(int(round(float(x[2]))))
 
-					for l in range(20):
-							nums.append(-1)
-					name = '.'.join(i.split('.')[0:2])
-					#o.write('%s' % i[:-11])
-					o.write('%s' % name)
-					for k in nums:
-						o.write('\t%s' % k)
-					o.write('\n')
-						
-			o.close() 
-			
+						for k in range(8):
+								nums.append(-1)
+
+						for l in range(20):
+								nums.append(-1)
+						name = '.'.join(i.split('.')[0:2])
+						#o.write('%s' % i[:-11])
+						o.write('%s' % name)
+						for k in nums:
+							o.write('\t%s' % k)
+						o.write('\n')
+
+
 			if warnings > 0:
 					print "There were %d warnings" % warnings
-			
+
+	def grayscale_handling(self):
+		"""
+		This method ensures that grayscaled images are handled properly by AutoTrace
+		"""
+		path = os.path.join(os.getcwd(), 'Subject1', 'IMAGES')
+		for f in os.listdir(path):
+			# bandaid for Matlab stuff
+			# even if the image is grayscale, "pretend" it's actually RGB, so that Matlab handles it properly...
+			if f.endswith('jpg') or f.endswith('JPG'):
+				source_name = os.path.join(path, f)
+				im = Image.open(source_name).convert("RGB")
+				im.save(source_name)
+
 	def configdir(self):
 			if not os.path.isdir('Subject1'):
 					os.makedirs('Subject1/IMAGES/')
 					os.mkdir('traces')
-		
+
 			self.formatAutoTrace()
 
 			tracecount = 0
@@ -138,8 +152,10 @@ class TkFileDialogExample(Tkinter.Frame):
 					subprocess.Popen(['cp', 'ROI_config.txt', 'Subject1/'])
 			if os.path.isfile('../ROI_config.txt'):
 					subprocess.Popen(['cp', '../ROI_config.txt', 'Subject1/'])
-					
-			
+
+			# handle grayscale images
+			self.grayscale_handling()
+
 			tkMessageBox.showinfo("Success", "moved {0} images and {1} traces".format(jpgcount, tracecount))
 			if jpgcount != tracecount:
 					print '\033[91m' + 'Warning: There is a mismatch in number of images and number of traces' + '\033[0m'
@@ -148,4 +164,3 @@ if __name__=='__main__':
 	root = Tkinter.Tk()
 	TkFileDialogExample(root).pack()
 	root.mainloop()
-	
